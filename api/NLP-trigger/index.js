235fbd3e552a203ff6f8f813e2b14802e944fda6
priya <blpriya1503@gmail.com>
2022-03-23 12:24:52 +0530

    change add

--- api/NLP-trigger/index.js
+++ api/NLP-trigger/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var url  = "https://sl4-nlp-api.cognitiveservices.azure.com//text/analytics/v3.2-preview.1/sentiment?opinionMining=true"
+    var url  = "https://sl4-nlp-api.cognitiveservices.azure.com/text/analytics/v3.2-preview.1/sentiment?opinionMining=true"
     var config = {
         headers: {
             "Ocp-Apim-Subscription-Key": "8c782ff3fab941e0b035ae576dacbb04"
